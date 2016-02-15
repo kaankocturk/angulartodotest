aa288a1fb96f1504b22b4e3d5072927dff0b63ad
kaankocturk <kkocturk1@babson.edu>
2016-02-15 11:03:37 -0800

    whatever

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ mongoose.connect(mongoUrl, function(err){
 var app = express();
 
 // view engine setup
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
